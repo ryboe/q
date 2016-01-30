f6052392cf128974832871091dd5a8e635277dd4
variadico <variadico@users.noreply.github.com>
2016-01-29 20:33:13 -0800

    Initial commit

---
+++ q.go
@@ -0,0 +1,36 @@
+package j
+
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	LogFile = "/var/log/j.log"
+)
+
+func Println(a ...interface{}) {
+	fd, err := os.OpenFile(LogFile, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+
+	_, err = fmt.Fprintln(fd, a...)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Printf(format string, a ...interface{}) {
+	fd, err := os.OpenFile(LogFile, os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+	defer fd.Close()
+
+	_, err = fmt.Fprintf(fd, format, a...)
+	if err != nil {
+		panic(err)
+	}
+}
