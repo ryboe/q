88e3d81a0cd289dea575d733f856afc63dc264ee
y0ssar1an <ryanboehning@gmail.com>
2016-02-15 13:55:59 -0800

    add safetime package

---
+++ internal/safetime/safetime.go
@@ -0,0 +1,48 @@
+package safetime
+
+import (
+	"sync"
+	"time"
+)
+
+type Time struct {
+	sync.Mutex
+	tm time.Time
+}
+
+func New() *Time {
+	return &Time{sync.Mutex{}, time.Time{}}
+}
+
+func Since(t *Time) time.Duration {
+	t.Lock()
+	defer t.Unlock()
+	return time.Now().Sub(t.tm)
+}
+
+func (t *Time) SetNow() {
+	t.Lock()
+	defer t.Unlock()
+	t.tm = time.Now()
+}
+
+type Timer struct {
+	sync.Mutex
+	time.Timer
+}
+
+func NewTimer(d time.Duration) *Timer {
+	return &Timer{sync.Mutex{}, *time.NewTimer(d)}
+}
+
+func (tmr *Timer) Reset(d time.Duration) bool {
+	tmr.Lock()
+	defer tmr.Unlock()
+	return tmr.Reset(d)
+}
+
+func (tmr *Timer) Stop() bool {
+	tmr.Lock()
+	defer tmr.Unlock()
+	return tmr.Stop()
+}
