086629c45e4ca64159a71125e1ccb40cd8c0e427
Ryan Boehning <ryan.boehning@apcera.com>
2016-11-06 14:31:10 -0800

    Make it look better in godoc

--- doc.go
+++ doc.go
@@ -1,12 +1,13 @@
-// Package q provides quick and dirty debugging output for tired programmers.
-// Just type Q(foo)
-// It's the fastest way to print a variable. Typing Q(foo) is easier than
-// fmt.Printf("%#v whatever"). The output is easy on the eyes, with
-// colorization, pretty-printing, and nice formatting. The output goes to
-// the $TMPDIR/q log file, away from the noisy output of your program. Try it
-// and give up using fmt.Printf() for good!
-//
-// For best results, import it like this:
-// import . "github.com/y0ssar1an/q"
+/*
+Package q provides quick and dirty debugging output for tired programmers.
 
+q.Q() is a fast way to pretty-print variables. It's easier than typing
+fmt.Printf("%#v", whatever). The output will be colorized and nicely formatted.
+The output goes to $TMPDIR/q, away from the noise of stdout.
+
+q exports a single Q() function. This is how you use it:
+    import "github.com/y0ssar1an/q"
+    ...
+    q.Q(a, b, c)
+*/
 package q
